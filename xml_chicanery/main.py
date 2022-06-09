2b46205f50ac8948c940d4f119ca1ee7cf7f7511
Ginkies <joe@neatleaf.com>
2022-06-09 12:38:34 -0700

    updated main.py with some example code

--- xml_chicanery/main.py
+++ xml_chicanery/main.py
@@ -1,4 +1,5 @@
 
+from re import sub
 import pretty_errors
 import pprint
 from xml_chicanery.model.from_xml import Jdbor
@@ -22,8 +23,27 @@ config = ParserConfig(
     fail_on_unknown_attributes=False,)
 
 parser = XmlParser(context= XmlContext(), config=config)
-jdbor = parser.parse(xml_path, Jdbor)
+jdbor:Jdbor = parser.parse(xml_path, Jdbor)
 
 # print the first 2 elements from the date_lg.xml
 for disorder in jdbor.disorder_list.disorder[:2]:
-    pp.pprint(disorder.__dict__)
+    # pp.pprint(disorder.__dict__)
+    # pp.pprint(disorder)
+    # print(f'id: {disorder.id}')
+    # print(f'orphacode: {disorder.orpha_code}')
+    result = []
+    result.append(disorder.id)
+    result.append(disorder.orpha_code)
+    result.append(disorder.name.value)
+    insert_command = f"INSERT INTO disorder (id, OrphaCode, name) VALUES ({result});"
+    # for summary_information in disorder.summary_information_list.summary_information:
+    #     for synonym in disorder.synonym_list.synonym:
+    #         sub_result = []
+    #         sub_result.append(disorder.id)
+    #         sub_result.append(disorder.orpha_code)
+    #         sub_result.append(disorder.summary_information)
+    #         sub_result.append(disorder.synonym)
+    #         result.append(sub_result)
+    print(result)
+
+
